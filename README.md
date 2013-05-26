f41cda0e9f2696ad395c7f122125fca931b38b3b
Mitchell Stanley <ubuntumitch@gmail.com>
2013-05-26 15:56:34 +0100

    Code rewrite to work with PHPUnit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Canvas
+
+Canvas is a simple PHP class for creating thumbnails and
+manipulating image sizes with the GD Library.
+
--- example.php
+++ example.php
@@ -1,25 +1,17 @@
-<?php
-require 'canvas.class.php';
+<?php 
+require_once 'vendor/autoload.php';
 
 $canvas = new \Acoustep\Canvas();
 
-$canvas->width(500)
-       ->height(500)
-       ->add('image', 'test.jpg', array('scale' => 'best'))
-       ->add('shape', 'square', array('color' => '#ff0000',
-                            'x' => 0,
-                            'y' => 0,
-                            'w' => 250,
-                            'h' => 250,
-                            'transparency' => 40))
-       ->add('image', 'test2.jpg', array('scale' => 'width',
-                                         'x' => 'right',
-                                         'y' => 'bottom'))
-       ->add('shape', 'square', array('color' => '#00ff00',
-                                      'x' => 150,
-                                      'y' => 150,
-                                      'w' => 100,
-                                      'h' => 100,
-                                      'transparency' => 90))
-       ->output('test3')
+
+$canvas->width(900)
+       ->height(900)
+       ->image('test.jpeg', array('scale' => 'width',
+                                  'x' => 'left',
+                                  'y' => 'bottom'))
+//       ->image('test.jpeg', array('scale' => 'width',
+//                                  'x' => 500,
+//                                  'y' => 500))
+       ->output('test4')
+       ->filetype('png')
        ->create();
